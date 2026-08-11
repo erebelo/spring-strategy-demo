4e7d1e2725c112cf5b301a25ebf93ee0c6d00a63
erebelo <eduldrebelo@gmail.com>
2026-08-10 22:11:14 -0300

    Removing unnecessary LocaleConfig bean and setting up messages to English by SpringStrategyDemoApplication

--- src/main/java/com/erebelo/springstrategydemo/SpringStrategyDemoApplication.java
+++ src/main/java/com/erebelo/springstrategydemo/SpringStrategyDemoApplication.java
@@ -1,5 +1,6 @@
 package com.erebelo.springstrategydemo;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStrategyDemoApplication {
 
     public static void main(String[] args) {
+        // Ensures validation and other localized messages are consistently in English.
+        Locale.setDefault(Locale.US);
+
         SpringApplication.run(SpringStrategyDemoApplication.class, args);
     }
-
 }
